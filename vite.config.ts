e7d79fbf19aa40774b23dc87937729351c83ffa4
Aidanu504 <AidanUrbina504@gmail.com>
2025-02-26 00:14:05 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: process.env.VITE_BACKEND_URL,
+        target: "https://emdc-backend-mahe5.ondigitalocean.app",
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
